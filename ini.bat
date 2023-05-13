12f075aa85aec341ba91d6768018e6e1191bbdb5
t5 <t5@mail>
2023-05-14 00:42:23 +0900

    comment

--- ini.bat
+++ ini.bat
@@ -1,7 +1,17 @@
 echo "# lege" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:koiwairise/lege.git
+REM git init
+REM git add .
+REM git commit -m "first commit"
+REM git branch -M main
+REM git remote add origin git@github.com:koiwairise/lege.git
+
+REM ssh-keygen -t rsa -b 4096 -C "t5"
+
+REM pushd C:\Users\user\.ssh
+REM set TARGET_HOST=github.com
+REM ssh-keygen -R %TARGET_HOST%
+REM ssh-keyscan -H %TARGET_HOST% >> known_hosts
+REM popd
+
+git commit -am"comment"
 git push -u origin main
